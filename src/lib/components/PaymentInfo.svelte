47702b52b57e52510b7ac5c450b6b15de1f4a256
Herman Gudesen <74401965+Hermangud@users.noreply.github.com>
2025-07-05 11:30:35 +0200

    add qr code and kid to payment

--- src/lib/components/PaymentInfo.svelte
+++ src/lib/components/PaymentInfo.svelte
@@ -5,11 +5,12 @@
 <div class="row g-4">
 	<div class="col-md-6">
 		<div class="card h-100">
-			<div class="card-body">
+			<div class="card-body mx-a">
 				<h3 class="card-title">Vipps</h3>
 				<p class="card-text">
 					Send <strong>250 kr</strong> kr til <strong>978 18 141</strong>
 				</p>
+				<img class="w-50" src="/static/qr-vipps.svg" alt="QR kode til Vipps" />
 			</div>
 		</div>
 	</div>
--- src/lib/components/StatusChecker.svelte
+++ src/lib/components/StatusChecker.svelte
@@ -1,23 +1,28 @@
 <script lang="ts">
 	import { supabase } from '$lib/supabaseClient';
+	import PaymentInfo from './PaymentInfo.svelte';
 
 	let emailToSearch = $state<string>('');
 	let isLoading = $state<boolean>(false);
 	let searchStatus = $state<'idle' | 'found' | 'not_found'>('idle');
 	let errorMessage = $state<string>('');
 	let paidStatus = $state<boolean | null>(null);
+	let kidNumber = $state<number | null>(null);
 
-	async function handleStatusCheck(): Promise<void> {
+	async function handleStatusCheck(event: Event): Promise<void> {
+		event.preventDefault();
 		if (!emailToSearch) return;
 
 		isLoading = true;
 		searchStatus = 'idle';
 		errorMessage = '';
-		paidStatus = null; // Nullstill betalingsstatus ved nytt søk
+		paidStatus = null;
+		kidNumber = null;
+
 		try {
 			const { data, error } = await supabase
 				.from('public_guest_status') // Spør mot det sikre viewet
-				.select('paid') // Henter betalingsstatus
+				.select('paid, kid') // Henter betalingsstatus
 				.eq('email', emailToSearch)
 				.single();
 
@@ -27,6 +32,7 @@
 				searchStatus = 'found';
 				// NY: Lagrer betalingsstatus fra svaret
 				paidStatus = data.paid;
+				kidNumber = data.kid;
 			}
 		} catch (error: any) {
 			errorMessage = 'En teknisk feil oppstod. Prøv igjen.';
@@ -79,11 +85,11 @@
 			</div>
 		{:else}
 			<div class="alert alert-warning mt-3">
-				⚠️ Ja, vi har mottatt din påmelding, men mangler registrert betaling.{kidNumber}
+				⚠️ Ja, vi har mottatt din påmelding, men mangler registrert betaling.
 			</div>
+			<PaymentInfo {kidNumber} />
 		{/if}
 	{/if}
-
 	{#if searchStatus === 'not_found'}
 		<div class="alert alert-danger mt-3">
 			❌ Nei, vi finner ingen påmelding med denne e-postadressen.
